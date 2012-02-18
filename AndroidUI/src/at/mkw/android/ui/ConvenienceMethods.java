faefd23ed31de978fbc1ffadb4f3b2357682d6ec
tobias <tgeibinger@gmail.com>
2012-02-18 18:57:26 +0100

    added implizit intent launching

--- AndroidUI/src/at/mkw/android/ui/ConvenienceMethods.java
+++ AndroidUI/src/at/mkw/android/ui/ConvenienceMethods.java
@@ -1,5 +1,10 @@
 package at.mkw.android.ui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+
+import xml.XmlParser;
 import model.ModelUtils;
 import model.Scenario;
 import model.Slider;
@@ -8,6 +13,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ConvenienceMethods {
   public static String CUR_ID = "aktID";
@@ -37,6 +43,59 @@ public class ConvenienceMethods {
     newIntent.putExtras(b);    
     return newIntent;
   }
+  
+  public static void startScenarioFromFolder(String folder, Context context){
+		String xmlPath = folder;
+
+		File workingDir = new File(xmlPath);
+		File xsdFile = null;
+		File xmlFile = new File(workingDir.getAbsolutePath() + "/" + workingDir.getName() + ".xml");
+
+		if (!xmlFile.exists()) {
+			ConvenienceMethods.showAlertDialog(context, "Couldn't find equally named xml file inside folder");
+		}
+
+		File[] xsdFiles = workingDir.listFiles(new FilenameFilter() {
+			public boolean accept(File file, String name) {
+				if (name.endsWith(".xsd")) {
+					return true;
+				}
+				return false;
+			}
+
+		});
+
+		if (xsdFiles.length != 1) {
+			if (xsdFiles.length == 0) {
+				ConvenienceMethods.showAlertDialog(context, "Couldn't find schema file inside folder");
+			} else {
+				ConvenienceMethods.showAlertDialog(context, "Only one xsd file allowed inside folder");
+			}
+		} else {
+			xsdFile = xsdFiles[0];
+		}
+		
+		if (xmlFile.exists() && xsdFile != null) {
+			try {
+				ConvenienceMethods.SZENARIO = XmlParser.parseFile(null, xmlFile);
+				
+				if (ConvenienceMethods.SZENARIO.getDevice().equals(Scenario.Device.MOBILE)) {
+					ConvenienceMethods.IMAGE_DIR = xmlPath + "/";
+					Intent newIntent = ConvenienceMethods.getNewIntent(ConvenienceMethods.SZENARIO.getStartScreen(), context);
+					context.startActivity(newIntent);
+				}
+				else {
+					ConvenienceMethods.showAlertDialog(context, "This scenario is not supposed to run on a mobile device");
+				}
+				
+			} catch (FileNotFoundException ex) {
+				ConvenienceMethods.showAlertDialog(context, "Couldn't find file " + xmlFile);
+			} catch (Exception ex) {
+				Log.e("tester",ex.getMessage(),ex);
+				ConvenienceMethods.showAlertDialog(context, ex.getMessage());
+			}
+		}
+	}
   
   public static void showAlertDialog(Context con, String msg) {
 	  AlertDialog.Builder error = new AlertDialog.Builder(con);
